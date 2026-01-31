0bd483744ca96be53963153f74f65e2973b2d9be
aleciaortiz923-star <alecia.ortiz923@gmail.com>
2026-01-30 21:43:10 -0500

    update project

--- index.js
+++ index.js
@@ -1,3 +1,45 @@
-//template_gx5t3dn  
-//service_qcc1ss7
-//asW80CxBjlWa4UQq9
+let isModalOpen = false;
+let contrastToggle = false;
+
+function toggleContrast() {
+    contrastToggle = !contrastToggle;
+    if (contrastToggle) {
+        document.body.classList += " dark-theme";
+    }
+    else {
+        document.body.classList.remove("dark-theme");
+    }
+   
+}
+
+function contact(event) {
+    event.preventDefault();
+    const loading =document.querySelector('.modal__overlay--loading');
+    const success =document.querySelector('.modal__overlay--success');
+     loading.classList += " modal__overlay--visible";
+   emailjs
+   .sendForm(
+    'service_xz00ptf', 'template_lbbsr8e', 
+    event.target,
+   'asW80CxBjlWa4UQq9'
+   )
+    .then( ()  => {
+         loading.classList.remove("modal__overlay--visible");
+        success.classList +=  " modal__overlay--visible";
+    } ).catch( () => {
+        loading.classList.remove("modal__overlay--visible");
+        alert(
+            "The email service is temporarily unavailable. Please contact me directly at alecia.ortiz923@gmail.com"
+        );
+    } );
+    }
+
+    
+    function toggleModal() {
+        if (isModalOpen) {
+            isModalOpen = false;
+            return document.body.classList.remove("modal--open");
+        }
+        isModalOpen = true;
+        document.body.classList += " modal--open";
+    }
